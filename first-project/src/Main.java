94476f960448d09dba5eaba0cb74eb741b3dbf15
Anastasiia <anastasiakosolapova1@gmail.com>
2024-11-18 18:54:53 +0500

    Для каждого задания создан метод

--- first-project/src/Main.java
+++ first-project/src/Main.java
@@ -1,49 +1,71 @@
 public class Main {
 
+    // задание 1
+    // посчитать перимерт
+    public static int calculatePerimeter(int length, int width) {
+        int perimeter = (length + width) * 2;
+        System.out.println("Периметр прямоугольника равен: " + perimeter);
+        return perimeter;
+    }
+    // Задание 2
+    public static boolean isIdentyfying(int number) {
+        int even = 0;
+        if (number % 2 != even) {
+        System.out.println("Число ялвяется нечетным: ");
+        }
+        else {
+            System.out.println("Число ялвяется четным: ");
+        }
+        return true;
+    }
+    // Задание 3 не сделано
+    public static double calculateArithmeticmean(int[] numbers) {
+        double sum = 0;
+        for (int i = 0; i < numbers.length; i++) {
+            sum = sum + numbers[i];
+            }
+        double mean = sum / numbers.length;
+        System.out.println(mean);
+        return mean;
+    }
+    // Задание 4
+    public static int [] identifyMaxMin(int [] numbers) {
+        int max = numbers [0];
+        int min = numbers [0];
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i] < min) {
+                min = numbers[i];
+            }
+            if (numbers[i] > max) {
+                max = numbers[i];
+            }
+        }
+        int[] maxandmin = {min,max};
+        System.out.println("Минимальное значение: " + maxandmin[0]);
+        System.out.println("Максимальное значение: " + maxandmin[1]);
+        return maxandmin;
+    }
+
     public static void main(String[] args) {
-        // Задание 1
-        System.out.println("Задание 1");
-        int lenght = 7;
-        int width = 15;
-        System.out.println("Периметр прямоугольника равен:" + (lenght + width) * 2);
-        System.out.println("Площаль прямоугольника равна:" + lenght * width);
+        calculatePerimeter(45, 7);
+        calculatePerimeter(4, 8);
 
         // Задание 2
         System.out.println("Задание 2");
-        int s = 8;
-        int v = s % 2;
-        if (v == 1) {
-            System.out.println("Число s является нечетным");
-        } else {
-            System.out.println("Число s является четным");
-        }
+        isIdentyfying(6);
+        isIdentyfying(9);
 
         // Задание З
         System.out.println("Задание 3");
-        int[] numbers = {6, 7, 8, 9, 10};
-        int sum = 0;
-        for (int i = 0; i < numbers.length; i++) {
-            sum = sum + numbers[i];
-        }
-        System.out.println(sum / numbers.length);
+        int[] massive = {7,8,9,10,12};
+        calculateArithmeticmean(massive);
 
         // Задание 4
+        // вынести в метод который возвращает массив int[], int[0] - min, int[1] - max
         System.out.println("Задание 4");
-        int[] array = {4, 5, 3, 2, 7, 8, 9, 65, 45, 76};
-        int min = array[0];
-        int max = array[0];
-        {for (int i = 1; i < array.length; i++) {
-            if (array[i] < min) {
-                min = array[i];
-            }
-            if (array[i] > max) {
-                max = array[i];
-            }
-        }
-            System.out.println("Минимальное значение массива: " + min);
-            System.out.println("Максимальное значение массива: " + max);
+        int[] numbers = {1,45,3,2,87,9,65,34,90,12};
+        identifyMaxMin(numbers);
         }
     }
-}
 
 
